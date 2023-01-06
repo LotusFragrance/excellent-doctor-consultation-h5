6d0b20b32521ff5d580b27d19d7de0ba45f58fdc
hexiang <2369275200@qq.com>
2023-01-06 16:54:17 +0800

    用户中心

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,68 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import { useUserStore } from '@/stores/index'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
-      component: () => import('@/views/Login/index.vue')
+      component: () => import('@/views/Login/index.vue'),
+      meta: { title: '登录' }
+    },
+    {
+      path: '/',
+      component: () => import('@/views/Layout/index.vue'),
+      redirect: '/home',
+      children: [
+        {
+          path: '/home',
+          component: () => import('@/views/Home/index.vue'),
+          meta: { title: '首页' }
+        },
+        {
+          path: '/article',
+          component: () => import('@/views/Article/index.vue'),
+          meta: { title: '健康百科' }
+        },
+        {
+          path: '/notify',
+          component: () => import('@/views/Notify/index.vue'),
+          meta: { title: '消息通知' }
+        },
+        {
+          path: '/user',
+          component: () => import('@/views/User/index.vue'),
+          meta: { title: '个人中心' }
+        }
+      ]
     }
   ]
+})
+
+// 访问权限控制
+router.beforeEach(async (to, from, next) => {
+  document.title = `优医问诊-${to.meta.title || ''}`
+  // 用户仓库
+  const store = useUserStore()
+  // 不需要登录的页面，白名单
+  const whiteList = ['/login', '/register']
+  // 有token
+  if (store.userInfo?.token) {
+    // 跳转的页面是登录页面
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    // 没有token
+    // 是否位白名单
+    // 在白名单中
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
 })
 
 export default router
